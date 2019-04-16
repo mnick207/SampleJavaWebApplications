0a5c64df1c4a71d3e355d99e83935493f7a7b2fa
Sandeep <Sandeep@192.168.1.3>
2019-04-17 02:00:29 +0530

    Insert Student in Database via JDBC

--- StudentManagementSystem/src/com/company/studentmanagementsystem/servlet/StudentServlet.java
+++ StudentManagementSystem/src/com/company/studentmanagementsystem/servlet/StudentServlet.java
@@ -1,6 +1,13 @@
 package com.company.studentmanagementsystem.servlet;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -46,10 +53,77 @@ public class StudentServlet extends HttpServlet {
 			System.out.println("In Register Student processor block");
 			System.out.println("Displaying Student Registration Form to user");
 			
-			//jdbc call
-			//jdbc returned resultset/java collection
-			//forward to jsp
+			int noOfRowsAffected = 0;
+			Connection connection = null;
+					
+			//retrieve request parameters
+			String firstName = request.getParameter("firstName");
+			String lastName = request.getParameter("lastName");
+			String emailID = request.getParameter("emailId");
+			String contactNumber = request.getParameter("contactNumber");
+			String city = request.getParameter("city");
+			String state = request.getParameter("state");
+			String country = request.getParameter("country");
+			String course = request.getParameter("course");
+			String dateOfBirthString = request.getParameter("dateOfBirth");
+			long zipcode = Long.parseLong(request.getParameter("zipcode"));
+			System.out.println("Date of Birth from input is: "+dateOfBirthString);
+			java.util.Date utilDateOfBirth = null;
+			try {
+				utilDateOfBirth = new SimpleDateFormat("yyyy-MM-dd").parse(dateOfBirthString);
+			} catch (ParseException e) {
+				System.out.println("Error occured while Parsing Date from input!!");
+			}
+			Date dateOfBirth = new Date(utilDateOfBirth.getTime());
+			System.out.println("SQL Date of Birth is: "+dateOfBirth.toString());
 			
+			
+			//jdbc statements
+			  
+			try {
+				//load the driver class
+				Class.forName("oracle.jdbc.driver.OracleDriver");
+				
+				//create  the connection object  
+				connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","tiger");
+				
+				//create insert query
+				String insertStudentQuery = "INSERT INTO students VALUES(students_student_id_seq.nextval, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+				
+				PreparedStatement preparedStatement = connection.prepareStatement(insertStudentQuery);
+				preparedStatement.setString(1, firstName);
+				preparedStatement.setString(2, lastName);
+				preparedStatement.setString(3, emailID);
+				preparedStatement.setString(4, contactNumber);
+				preparedStatement.setString(5, city);
+				preparedStatement.setString(6, state);
+				preparedStatement.setString(7, country);
+				preparedStatement.setLong(8, zipcode);
+				preparedStatement.setDate(9, dateOfBirth);
+				preparedStatement.setString(10, course);
+				
+				noOfRowsAffected = preparedStatement.executeUpdate();
+				
+			} catch (ClassNotFoundException e) {
+				System.out.println("Error occured is: "+e.getMessage());
+			} catch (SQLException e) {
+				System.out.println("Error occured is: "+e.getMessage());
+			}finally {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					System.out.println("Exception in Closing DB Connection!!");
+				}
+			}
+			
+			//jdbc returned resultset/java collection
+			if(noOfRowsAffected == 1) {
+				System.out.println("Insertion is Successful!!");
+			}else {
+				System.out.println("Insertion has Failed!!");
+			}
+			
+			//forward to jsp
 			RequestDispatcher requestDispatcher = request.getRequestDispatcher("/WEB-INF/views/studentRegistrationResponse.jsp");
 			requestDispatcher.forward(request, response);
 			
